f6b42fb9b43aadf5b5e0ced2d9e831722a65f4a5
Jason Thompson <jason@jasont.us>
2020-05-30 09:43:33 -0400

    build node class

--- node.rb
+++ node.rb
@@ -1,6 +1,19 @@
 class Node
+  attr_accessor :value, :index, :next
+  @@count = 0
 
-  def initialize
-    
+  def initialize(value = nil, index = 0)
+    @value = value
+    @index = index
+    @next = nil
+    @@count += 1
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.remove
+    @@count -= 1
   end
 end
